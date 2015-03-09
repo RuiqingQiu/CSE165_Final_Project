9945918aaab1dac44f1b0656861a389958cb4736
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-03-09 14:48:55 -0700

    first commit, a scene with cube and some environment

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float y = Input.GetAxis ("Jump") * Time.deltaTime * speed;
+		float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		transform.Translate(x, y, z);
+	}
+}
